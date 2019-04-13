50c011aa9c2fbbe90627cc7b699300e9bac93b29
Karol Kotowicz <kkotowiczz@gmail.com>
2019-04-13 14:59:46 +0200

    correct tortoise race

--- scala/tortoise-race/Sol.scala
+++ scala/tortoise-race/Sol.scala
@@ -1,13 +1,13 @@
 object Sol {
   def race(v1: Int, v2: Int, g: Int): Array[Int] = {
+    val difference = g.toDouble / (v2 - v1).toDouble
     if(v1 >= v2) Array(-1, -1, -1)
-    else if (g < 0) Array(1,2, 3)
-    else race(v1, v2, (v1 + g) - v2)
+    else Array(difference.toInt, (difference * 60 % 60).toInt, (difference * 3600 % 60).toInt)
   }
 
 
   def main(args: Array[String]): Unit = {
-    val race1 = race(30, 50, 450)
+    val race1 = race(30, 50, 5436)
     race1.foreach(println)
   }
 }
